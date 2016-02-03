f7e95e038d7bf397a1a955b66fb56ea7c9a04f81
atousley <annette.tousley@gmail.com>
2016-02-03 09:07:52 -0600

    final assignment

--- app.js
+++ app.js
@@ -5,13 +5,45 @@ var scout = ["Scout", "6243", "74750", 5];
 
 var employees = [atticus, jem, boo, scout];
 
+// Object.setPrototypeOf(employees, Object.prototype);
+// console.log(employees);
+
+function Employee(name, empNumber, currentSalary, rating) {
+	this.name = name;
+	this.empNumber = empNumber;
+	this.currentSalary = currentSalary;
+	this.rating = rating;
+}
+
+atticus = new Employee(atticus[0], atticus[1], atticus[2], atticus[3]);
+jem = new Employee(jem[0], jem[1], jem[2], jem[3]);
+boo = new Employee(boo[0], boo[1], boo[2], boo[3]);
+scout = new Employee(scout[0], scout[1], scout[2], scout[3]);
+
+var employees = [atticus, jem, boo, scout];
+console.log(employees);
+
+// function empConvert(array) {
+// 	for(var i = 0; i < array.length; i++) {
+// 		this.name = array[i][0];
+// 		this.empNumber = array[i][1];
+// 		this.currentSalary = array[i][2];
+// 		this.rating = array[i][3];
+// 	}
+// 	console.log(employees);
+// }
+
+// empConvert(employees);
+// Why can't I figure out how to pass an array of arrays into a constructor?!!!
+
+
 function calculateSTI(empInfo) {
-	var name = empInfo[0];
-	var empNumber = empInfo[1];
-	var currentSalary = Math.round(parseFloat(empInfo[2]));
-	var rating = empInfo[3];
+	var name = empInfo.name;
+	var empNumber = empInfo.empNumber;
+	var currentSalary = Math.round(parseFloat(empInfo.currentSalary));
+	var rating = empInfo.rating;
 	
-	var processedEmployee = [];
+	var processedEmployee;
 	var bonus = 0;
 	var bonusPercentage = 0;
 	var adjSalary = currentSalary;	// base + STI
@@ -39,21 +71,29 @@ function calculateSTI(empInfo) {
 
 	bonusPercentage = adjustBonusPercentage(empNumber, bonusPercentage, currentSalary);
 	
-	// build processed array
-	processedEmployee[0] = name;
-	processedEmployee[1] = bonusPercentage;
-
 	bonus = Math.round(bonusPercentage * currentSalary);
 	adjSalary = currentSalary + bonus;
 
-	processedEmployee[2] = adjSalary;
-	processedEmployee[3] = bonus;
+	// build processed array
+	function EmployeeSti(name, bonusPercentage, adjSalary, bonus) {
+		this.name = name;
+		this.bonusPercentage = bonusPercentage;
+		this.adjSalary = adjSalary;
+		this.bonus = bonus;
+	}
+	processedEmployee = new EmployeeSti(name, bonusPercentage, adjSalary, bonus);
+
+	// processedEmployee.name = name;
+	// processedEmployee. = bonusPercentage;
+
+	// processedEmployee[2] = adjSalary;
+	// processedEmployee[3] = bonus;
 
 	return processedEmployee;
 }
 
 function adjustBonusPercentage(empNumber, bonusPercentage, currentSalary) {
-	if(empNumber.length == 4) {
+	if(empNumber == 4) {
 		bonusPercentage += .05;
 	}
 
@@ -65,11 +105,12 @@ function adjustBonusPercentage(empNumber, bonusPercentage, currentSalary) {
 		bonusPercentage = .13;
 	}
 
-	return bonusPercentage;
+	return Math.round(bonusPercentage * 100) / 100;
 }
 
 
 for(var i = 0; i < employees.length; i++) {
+	
 	console.log(calculateSTI(employees[i]));
 
 }
